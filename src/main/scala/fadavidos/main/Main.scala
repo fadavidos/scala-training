d7f0aef992356e7ad7263f293ca66584e2d7f051
Fabian Osorio <fabianosorio@seven4n.com>
2021-07-28 11:56:58 -0500

    Future is eager

--- src/main/scala/fadavidos/main/Main.scala
+++ src/main/scala/fadavidos/main/Main.scala
@@ -2,6 +2,4 @@ package fadavidos.main
 
 object Main extends App {
 
-
-
 }
--- src/test/scala/fadavidos/async/FutureTest.scala
+++ src/test/scala/fadavidos/async/FutureTest.scala
@@ -1,21 +1,51 @@
 package fadavidos.async
 
-import org.scalatest.{Assertion, Assertions}
-import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.Assertion
+import org.scalatest.flatspec.{AnyFlatSpec, AsyncFlatSpec}
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.Future
+import scala.util.Random
 
-class FutureTest extends AsyncFlatSpec {
+class FutureTest extends AnyFlatSpec {
+
+  implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
 
   behavior of "Future"
 
-  it should "be created succesful" in {
+  it should "be eager" in {
+    var fruits = new ListBuffer[String]()
+    fruits += "pear"
+    def addFruit(fruit: String): Unit = {
+      fruits += fruit
+    }
+    Future(addFruit("apple"))
+    Thread.sleep(5000)
+    assert(fruits.contains("apple"))
+  }
+
+  it should "memorize its value" in {
+    val oneElementValidation: Int => Assertion = value => assert(value == 1)
+    val randomIntFuture: Future[Int] = Future(Random.nextInt())
+    val numberElements: Future[Int] = for {
+      randomOne <- randomIntFuture
+      randomTwo <- randomIntFuture
+      randomThree <- randomIntFuture
+    } yield {
+      List(randomOne, randomTwo, randomThree)
+        .distinct //All numbers are the same
+        .length // The length of the list with different values is 1
+    }
+    numberElements.map(oneElementValidation)
+  }
+
+  "succesful()" should "be created" in {
     val expectedGreeting = "hi"
     val validation: String => Assertion = value => assert(expectedGreeting ==value)
     Future.successful("hi").map(validation)
   }
 
-  it should "be created failed" in {
+  "failed()" should "be created" in {
     Future.failed[String](new RuntimeException("something was wrong!!"))
       .map[Assertion] { _ => fail() }
       .recover {
