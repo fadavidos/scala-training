ef9ca7bd83f040e01bc8654452e18ff2883732e9
Fabian Osorio <fadavidos@gmail.com>
2023-06-10 08:26:17 -0500

    added a new test suit to test scala basics and also added fold tests

---
+++ src/test/scala/fadavidos/FPTest.scala
@@ -0,0 +1,43 @@
+package fadavidos
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class FPTest extends AnyFlatSpec {
+
+  "type class" should "allow us to implement different behavior when we have different data types" in {
+
+    trait JsonSerializable[A] {
+      def toJson(value: A): String
+    }
+
+    object JsonSerializableInstances {
+      implicit val intJsonSerializable: JsonSerializable[Int] = (value: Int) => value.toString
+
+      implicit val stringJsonSerializable: JsonSerializable[String] = new JsonSerializable[String] {
+        override def toJson(value: String): String = s"""$value"""
+      }
+
+      implicit val doubleJsonSerializable: JsonSerializable[Double] = new JsonSerializable[Double] {
+        override def toJson(value: Double): String = value.toString
+      }
+    }
+
+    object JsonSyntax{
+      implicit class JsonSerializableOps[A](value: A)(implicit ev: JsonSerializable[A]){
+        def convert: String = ev.toJson(value)
+      }
+    }
+
+    import JsonSerializableInstances._
+    import JsonSyntax._
+
+    val jsonInt: String = 42.convert
+    val jsonDouble: String = 3D.convert
+    val jsonString: String = "hello".convert
+
+    assertResult("42")(jsonInt)
+    assertResult("3.0")(jsonDouble)
+    assertResult("hello")(jsonString)
+  }
+
+}
--- src/test/scala/fadavidos/ScalaBasicsTest.scala
+++ src/test/scala/fadavidos/ScalaBasicsTest.scala
@@ -23,8 +23,29 @@ class ScalaBasicsTest extends AnyFlatSpec {
     assertResult("!hello")(applyFormat("HeLLo", formatLowerCase))
   }
 
-  it should "be returned from a method" in {
+  /*
+   fold function is to aggregate values or transform the collection into a single value.
+   We have three fold functions: fold, foldLeft and foldRight
+   */
 
+  "fold left" should "sum all elements of a list" in {
+    val sum: (Int, Int) => Int = (num1, num2) => num1 + num2
+    val numbers = List(1, 2, 3, 4)
+    val result = numbers.foldLeft(0)(sum)
+    assertResult(10)(result)
+  }
+
+  "fold right" should "accumulate all values in a list of Strings" in {
+    val concat: (String, String) => String = (s1, s2) => s"$s2 $s1".trim
+    val names = List("one", "two", "three", "four")
+    val result = names.foldRight("")(concat)
+    assertResult("four three two one")(result)
+  }
+
+  "fold" should "accumulate all values of a list, and return the product" in {
+    val numbers = List(1, 2, 3, 4)
+    val result = numbers.fold(1)(_ * _)
+    assertResult(24)(result)
   }
 
 }
